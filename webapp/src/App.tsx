9ee6a6de70c809d79ce59c0b4737837f0781f12d
artem-badasyan <133artemka123@gmail.com>
2025-05-14 18:02:16 +0300

    Changed App.ts

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,19 @@
 export const App = () => {
-  return <h1>Application</h1>
+  const ideas = [
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'},
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'},
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'},
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'}
+ ]
+ return (
+  <div>
+    <div>Ideas</div>
+    {ideas.map((idea) => (
+      <div key={idea.nick}>
+        <h2>{idea.name}</h2>
+        <p>{idea.descriptions}</p>
+      </div>
+    ))}
+  </div>
+ )
 }
