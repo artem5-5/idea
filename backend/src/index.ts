61e497131f47e002fb2404431a1f3c05e9d2c2ce
artem-badasyan <133artemka123@gmail.com>
2025-05-14 17:58:29 +0300

    Created tRPC and endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
-import express from "express"
+import express from 'express'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRouter } from './trpc'
 
 const expressApp = express()
 expressApp.get('/ping', (req, res) => {
-    res.send('pong')
+  res.send('pong')
 })
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
+)
 expressApp.listen(3000, () => {
-    console.info('Listening at http://localhost:3000')
+  console.info('Listening at http://localhost:3000')
 })
