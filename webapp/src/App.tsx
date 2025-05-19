1a782ec85c5c356c63ec890218167613c6c3f092
artem5-5 <73910058+artem5-5@users.noreply.github.com>
2025-05-19 22:44:14 +0300

    Revert "Updated tRPC, added Query, monorepo, install pnpm, created AllIdeasPa…"

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,19 @@
-import { TrpcProvider } from "./lib/trpc"
-import { AllIdeasPage } from "./pages/AllIdeasPage"
-
 export const App = () => {
-  return (
-    <TrpcProvider>
-      <AllIdeasPage />
-    </TrpcProvider>
-  )
+  const ideas = [
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'},
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'},
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'},
+    { nick: 'cool-idea-nick-1', name: 'Idea 1', descriptions: 'Descriptions of idea 1...'}
+ ]
+ return (
+  <div>
+    <div>Ideas</div>
+    {ideas.map((idea) => (
+      <div key={idea.nick}>
+        <h2>{idea.name}</h2>
+        <p>{idea.descriptions}</p>
+      </div>
+    ))}
+  </div>
+ )
 }
